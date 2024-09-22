3bca1be0b53b88c49a3452d69db7639e1d5c33d1
Timka6434 <timka6434@mail.ru>
2024-09-22 23:38:27 +0500

    Initial commit

---
+++ Models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocketFinansistWPF.Models
+{
+    internal class Account
+    {
+        private string _id {  get; set; }
+        private string _accountName { get; set; }
+        private decimal _balance { get; set; }
+        private bool _isLocked { get; set; }
+        private DateTime _DataCreateAccount {  get; set; }
+
+        public Account() 
+        {
+            _id = Guid.NewGuid().ToString();
+            _accountName = "no name";
+            _balance = 0;
+            _isLocked = true;
+            _DataCreateAccount = DateTime.Now;
+        }
+        public string GetAccountName() => _accountName;
+        public void SetAccountName(string SetName) => _accountName = SetName;
+
+        public void SetBalance(decimal  Balance) => _balance = Balance;
+
+        public void UnlockAccount() => _isLocked = false;
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PocketFinansistWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PocketFinansistWPF.ViewModels
+{
+    internal partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private Account _currentAccount;
+
+        public MainViewModel()
+        {
+            CurrentAccount = new Account();
+        }
+
+        [RelayCommand]
+        private void ActivateAccount()
+        {
+            if (CurrentAccount == null)
+            {
+                MessageBox.Show("Аккаунт не создан!");
+                return;
+            }
+
+            if (CurrentAccount.GetAccountName() != null)
+            {
+                CurrentAccount.UnlockAccount();
+            }
+        }
+
+        [RelayCommand]
+        public void CreateAccount(string accountName)
+        {
+            if(string.IsNullOrWhiteSpace(accountName))
+            {
+                MessageBox.Show("Введите корректное имя для аккаунта!");
+                return;
+            }
+            CurrentAccount = new Account();
+        }
+
+
+    }
+}
